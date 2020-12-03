9d9e31d329a55aa09c71bddb3a6cbcac8a86eb82
Vladimir Luvolunov <luvolunov@gmail.com>
2020-12-03 21:12:34 +0900

    c-4: added eslint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { AppProps } from "next/app";
-
+import './globals.scss';
+import { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
