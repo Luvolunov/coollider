47e097fbe99d3c6b12b303ab23bfd8f4b5e75c0b
Suhanov Nick <nik.152002@mail.ru>
2020-12-05 16:36:57 +0300

    c-hot-fix: added highlight

--- shared/Input/index.tsx
+++ shared/Input/index.tsx
@@ -1,14 +1,14 @@
 import { useEffect, useRef, useState } from "react";
 import styles from "./index.module.scss";
 
-export default function Input({type,checkData,isValid,updateFormState})
+export default function Input({type,checkData,valid,updateFormState})
 {
    const [isActive,setActive] = useState(false); // по дефолту инпут неактивен
    const inputRef = useRef({value : ""});
     return(
         <label className={`${styles.label}`} >
             <input
-            ref={inputRef}
+            ref={inputRef} // связал инпут и стейт
             placeholder={type} // использую встроенный placeholder
             onBlur={() => {
                 setActive(Boolean(inputRef.current.value));
@@ -17,7 +17,7 @@ export default function Input({type,checkData,isValid,updateFormState})
             onFocus={() => setActive(true)}
             className={styles.input}
             />
-            <small className={`${styles.title} ${ isActive ? styles.active : ""} ${isValid ? styles.success : ""}`} >{type}</small> 
+            <small className={`${styles.title} ${ isActive ? styles.active : ""} ${valid ? styles.success : !valid && !!inputRef.current.value ? styles.error : ""} `} >{type}</small> 
             <span className={styles.line}></span>
         </label>
         
