bbd49ffd743b448c9be4b2b6db4980569214efcd
Suhanov Nick <nik.152002@mail.ru>
2020-12-05 10:02:16 +0300

    c-hot-fix: using ref and useRef with input

--- shared/Input/index.tsx
+++ shared/Input/index.tsx
@@ -1,25 +1,22 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "./index.module.scss";
 
 export default function Input({placeholder})
 {
-    const [isActive,setActive] = useState(false); // по дефолту инпут неактивен
-    const [value,setValue] = useState(""); // по дефотку значение инпута - пустая строка
-
-    useEffect(() => {
-    },[isActive])
-
+   const [isActive,setActive] = useState(false); // по дефолту инпут неактивен
+   const inputRef = useRef({value : ""});
+   
     return(
         <label className={`${styles.label}`} >
             <input
+            ref={inputRef}
             placeholder={placeholder}
             onBlur={() => setActive(false)}
             onFocus={() => setActive(true)}
-            onChange={e => setValue(e.target.value)}
             className={styles.input}
             />
             {/* решил использовать нативный placeholder инпута */}
-            <small className={`${styles.title} ${!isActive && value ? styles.active : ""}`} >{placeholder}</small> 
+            <small className={`${styles.title} ${ inputRef.current.value ? styles.active : ""}`} >{placeholder}</small> 
             <span className={styles.line}></span>
         </label>
     )
