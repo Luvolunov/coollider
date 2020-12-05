4d31a5674083c07d703de91ac6d5bdef86930e5f
Suhanov Nick <nik.152002@mail.ru>
2020-12-05 10:11:28 +0300

    c-hot-fix: input focus active, if it contains some text

--- shared/Input/index.tsx
+++ shared/Input/index.tsx
@@ -5,18 +5,17 @@ export default function Input({placeholder})
 {
    const [isActive,setActive] = useState(false); // по дефолту инпут неактивен
    const inputRef = useRef({value : ""});
-   
     return(
         <label className={`${styles.label}`} >
             <input
             ref={inputRef}
             placeholder={placeholder}
-            onBlur={() => setActive(false)}
-            onFocus={() => setActive(true)}
+            onBlur={() => setActive(Boolean(inputRef.current.value))}
+            onFocus={() => setActive(true) } // если в инпуте есть дата - он активен
             className={styles.input}
             />
             {/* решил использовать нативный placeholder инпута */}
-            <small className={`${styles.title} ${ inputRef.current.value ? styles.active : ""}`} >{placeholder}</small> 
+            <small className={`${styles.title} ${ isActive ? styles.active : ""}`} >{placeholder}</small> 
             <span className={styles.line}></span>
         </label>
     )
