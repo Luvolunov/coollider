9d415ba6ba1b155f2d94e23e2c3910adfe34019f
Никита Суханов <nik.152002@mail.ru>
2020-12-15 10:47:26 +0300

    c-refactored: comments added

--- shared/Sign/Sign.tsx
+++ shared/Sign/Sign.tsx
@@ -5,15 +5,13 @@ import Input from "../Input/Input";
 import { useEffect, useState } from "react";
 import {InputData,SignLink} from "../interface";
 import ChangeSign from "../ChangeSign/ChangeSign";
-// import { useFormik } from "formik";
-// import * as Yup from "yup";
-
+import { useFormik } from "formik";
+import { validationSchema } from "./check";
 // let schema = Yup.object().shape({
 //     name: Yup.string().required(),
 //     age: Yup.number().required(),
 //     email: Yup.string().email()
 // })
-        
 
 interface SignData {
     fields: Array <InputData>,
@@ -22,34 +20,34 @@ interface SignData {
 export default function Sign({fields,changeForm} : SignData)
 {
     const [fieldsState,updateForm] : [fieldsState : Array<InputData>,updateForm : Function] = useState(fields);
-    const [access,setAccess] = useState(false);
-
-
+	 const [access,setAccess] = useState(false);
+ 
 	 useEffect(() => setAccess(fieldsState.every(({valid}) => valid)) // если каждое поле валидное - есть доступ
 	 ,[fieldsState])
 
     return(
 		<div  className={styles.wrapper}>
 			<div>
-		<img className={styles.image} src="/coollider.png" alt=""/>
+		<img className={styles.image} src="/coollider.png" alt="Coollider"/>
   		</div>
-    <form className={styles.sign} noValidate action="">
+    <form className={styles.sign} noValidate action=""> 
         {
             fieldsState.map(({type, checkData,valid}) => <Input
             type={type}
             checkData={checkData}
             key={type}
             valid={valid}
-            updateFormState={(valid : boolean,inpType : string) =>  { 
+            updateFormState={(valid : boolean,inpType : string ) =>  { // обновление стейта формы 
                 updateForm( // после блюра инпута 
-                    produce(fieldsState,draft => draft
-                        .map(input => input.type != inpType ? input : {...input,valid})
+                    produce(fieldsState,draft => draft // draft - копия состояния формы
+                        .map((input) : InputData => input.type != inpType ? input : {...input,valid}) // если поймали изменяемый инпут - обновляем свойство valid
                     )
                 )                
             }}      
             />)
         }
         <Button
+		  type="submit"
         textContent="Войти"
 		  access={access}
         />
