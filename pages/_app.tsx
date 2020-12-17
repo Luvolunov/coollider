6ea4a90428e8a4c599771a1578a85819a52b81f5
Vladimir Luvolunov <luvolunov@gmail.com>
2020-12-17 13:28:22 +0900

    c-14: added meta tags for all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import './global.scss';
 import { AppProps } from 'next/app';
+import Head from "next/head";
+import React from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+      <>
+        <Head>
+            <meta charSet="UTF-8" />
+            <meta name="viewport"
+                  content="width=device-width, initial-scale=1.0, maximum-scale=5.0, minimum-scale=1.0" />
+            <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+            <meta name="theme-color" content="#C8DDF0" />
+        </Head>
+        <Component {...pageProps} />
+      </>
+  );
 }
