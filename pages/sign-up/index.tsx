b85f5a73dc16a7889d05db5be0b718a7a9aeb657
Suhanov Nick <nik.152002@mail.ru>
2020-12-28 13:58:49 +0300

    c-14: added registation page

---
+++ pages/sign-up/index.tsx
@@ -0,0 +1,50 @@
+import Form from "../../shared/components/form/form.component"
+import Input from "../../shared/components/input/input.component"
+import Logo from "../../shared/components/logo/logo.component"
+import { useForm } from "../../shared/hooks/useForm.hook"
+import Link from "next/link"
+import styles from "./sign-up.module.scss";
+import { SignUpSchema } from "./sign-up.schema"
+import Head from "next/head"
+import Button from "../../shared/components/button/button.component"
+
+export default function SignUpPage() {
+    const {handleInput,valid,values} = useForm(SignUpSchema);
+    console.log(values)
+    return(
+        <>
+        <Head>
+            <title>Зарегистрироваться в Coollider!</title>
+        </Head>
+        <main className={styles.page}>
+        <Form style = {{maxWidth: '500px'}}>
+            <Logo/>
+            <br/>
+            <br/>
+            <Input placeholder="Имя" onChange={handleInput} name="firstName" />
+            <br/>
+            <Input placeholder="Фамилия" onChange={handleInput} name="lastName"/>
+            <br/>
+            <Input placeholder="Почта" onChange={handleInput} name="email" />
+            <br />
+            <Input placeholder="Пароль" type="password" onChange={handleInput} name="password" />
+            <br />
+            <Input placeholder="" type="date" onChange={handleInput} name="dateOfBith"/>
+            <br />
+            <br/>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <Button type="button" disabled={!valid}>Зарегистрироваться</Button>
+          </div>
+          <br />
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <Link href="/sign-in">
+              <a>У меня есть аккаунт :)</a>
+            </Link>
+          </div>
+        </Form>
+        </main>
+        </>
+    )
+}
+
+
---
+++ pages/sign-up/sign-up.schema.ts
@@ -0,0 +1,21 @@
+import { checkDate } from './../../shared/validators/dateFormat.validator';
+import { ValidationSchema } from '../../shared/hooks/validation-schema.interface';
+import { isEmail } from '../../shared/validators/isEmail.validator';
+import { minLength } from '../../shared/validators/min-length.validator';
+export const SignUpSchema: ValidationSchema = {
+    firstName: {
+        validators: [minLength(2)]
+    },
+    lastName: {
+        validators: [minLength(2)]
+    },
+    email: {
+        validators: [isEmail]
+    },
+    password: {
+        validators: [minLength(8)]
+    },
+    dateOfBith: {
+        validators: [checkDate]
+    }
+}
