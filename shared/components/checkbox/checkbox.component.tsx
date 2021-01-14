a7a300269d100a5d29ed1fcd63a7f69f06a25d3f
Suhanov Nick <nik.152002@mail.ru>
2021-01-14 15:11:21 +0300

    c-25: added checkbox component

---
+++ shared/components/checkbox/checkbox.component.tsx
@@ -0,0 +1,17 @@
+import styles from "./checkbox.module.scss";
+import {CheckboxProps} from "../../props/checkbox.props";
+
+export function Checkbox(
+    {condition,...props} : CheckboxProps) {
+    return(
+        <label className={styles.label}>
+            <input
+            className={styles.checkbox}
+            type="checkbox"
+            {...props}
+            />
+            <span className={styles.condition}>{condition}</span>
+            <span className={styles.fake}></span>
+        </label>
+    )
+} 
