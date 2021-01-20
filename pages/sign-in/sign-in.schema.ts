223ead7d2765b4f6492738a39d517eb30d531958
Suhanov Nick <nik.152002@mail.ru>
2021-01-20 18:07:14 +0300

    c-refactor: leverage extra validators, added to schema

--- pages/sign-in/sign-in.schema.ts
+++ pages/sign-in/sign-in.schema.ts
@@ -1,8 +1,9 @@
 import { ValidationSchema } from '../../shared/hooks/validation-schema.interface';
 import { isEmail } from '../../shared/validators/isEmail.validator';
 import { minLength } from '../../shared/validators/min-length.validator';
+import { isStrongPassword } from '../../shared/validators/password.validator';
 
 export const SignInSchema: ValidationSchema = {
   email: [isEmail],
-  password: [minLength(8)],
+  password: [minLength(8),isStrongPassword],
 };
--- pages/sign-up/sign-up.schema.ts
+++ pages/sign-up/sign-up.schema.ts
@@ -3,12 +3,14 @@ import { ValidationSchema } from '../../shared/hooks/validation-schema.interface
 import { isEmail } from '../../shared/validators/isEmail.validator';
 import { minLength } from '../../shared/validators/min-length.validator';
 import { isChecked } from '../../shared/validators/checked.validator';
+import { isStrongPassword } from '../../shared/validators/password.validator';
+import { nameIsCorrect } from '../../shared/validators/name.validator';
 
 export const SignUpSchema: ValidationSchema = {
-    firstName: [minLength(2)],
-    lastName: [minLength(2)],
+    firstName: [minLength(2),nameIsCorrect],
+    lastName: [minLength(2),nameIsCorrect],
     email: [isEmail],
-    password: [minLength(8)],
+    password: [minLength(8),isStrongPassword],
     dateOfBirth: [checkDate],
     agreement: [isChecked]
 }
