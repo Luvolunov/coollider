8952455384d4c5adccd1c69432fe7e987c856910
Vladimir Luvolunov <luvolunov@gmail.com>
2021-02-10 13:00:30 +0900

    c-32: added adaptivity and name block

--- shared/components/header/header.component.tsx
+++ shared/components/header/header.component.tsx
@@ -5,7 +5,8 @@ export default function Header(){
     return(
         <header className={styles.header}>
             <h1 className={styles.title}>Курсы</h1>
-            <div>
+            <div className={styles.userInfo}>
+                <span className={styles.name}>Имя Фамилия</span>
                 <Image src="/user-icon.svg" width={45} height={45} alt="user" />
             </div>
         </header>
