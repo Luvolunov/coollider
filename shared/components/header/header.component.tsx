880324496956b29c3b423f319b5da7139c4cff66
Vladimir Luvolunov <luvolunov@gmail.com>
2021-02-10 12:45:05 +0900

    c-32: added right user icon for menu

--- shared/components/header/header.component.tsx
+++ shared/components/header/header.component.tsx
@@ -1,8 +1,13 @@
 import styles from "./header.module.scss";
+import Image from 'next/image';
 
 export default function Header(){
     return(
         <header className={styles.header}>
+            <h1 className={styles.title}>Курсы</h1>
+            <div>
+                <Image src="/user-icon.svg" width={45} height={45} alt="user" />
+            </div>
         </header>
     )
 }
