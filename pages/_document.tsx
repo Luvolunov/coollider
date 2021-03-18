27e5ead483bbc32d3b748b9c10b054b822266355
Vladimir Luvolunov <luvolunov@gmail.com>
2021-03-18 20:00:23 +0900

    cp-metrika: added Ya Metrika

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,11 +2,31 @@ import NextDocument, {
   Html, Head, Main, NextScript,
 } from 'next/document';
 
+const yaMetrika = `
+  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(74119825, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+`;
+
 export default class AppDocument extends NextDocument {
   render() {
     return (
       <Html lang="ru">
-        <Head />
+        <Head>
+          <script dangerouslySetInnerHTML={{ __html: yaMetrika }}></script>
+          <noscript>
+            <div>
+              <img src="https://mc.yandex.ru/watch/74119825" style={{ position: 'absolute', left: '-9999px' }} alt="" />
+            </div>
+          </noscript>
+        </Head>
         <body>
           <Main />
           <NextScript />
