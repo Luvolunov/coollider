aa6a9635d137d28d8f8f9ddec606593b2fce0ba9
Vladimir Ivanov <luvolunov@gmail.com>
2021-04-22 15:06:27 +0900

    cp-refactoring: refactored menu

--- shared/components/logo/logo.component.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Image from 'next/image';
-import styles from './logo.module.scss';
-
-export default function Logo() {
-  return <Image className={styles.logo} width={1972} height={1004} src="/coollider.png" alt="rocket" />;
-}
--- shared/components/menu/menu.component.tsx
+++ shared/components/menu/menu.component.tsx
@@ -1,52 +1,52 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import classnames from 'classnames';
 import styles from './menu.module.scss';
 
 export default function Menu() {
-  const notMobileItemClass = classnames(styles.menuItem, styles.notMobile);
   return (
-    <aside className={styles.aside}>
-      <div className={styles.topMenu}>
+    <nav className={styles.menu}>
+      <div className={styles.firstMenu}>
         <Link href="/courses">
-          <div className={styles.menuItem}>
-            <Image src="/icons/book.svg" width={38} height={38} alt="courses" />
-          </div>
+          <a className={styles.menuItem}>
+            <Image width={25} height={29} src="/icons/book.svg" />
+            <span className={styles.menuItemTitle}>Курсы</span>
+          </a>
         </Link>
-        <Link href="/courses">
-          <div className={styles.menuItem}>
-            <Image src="/icons/trophy.svg" width={38} height={38} alt="trophy" />
-          </div>
+        <Link href="/statistics">
+          <a className={styles.menuItem}>
+            <Image width={25} height={29} src="/icons/stats.svg" />
+            <span className={styles.menuItemTitle}>Статистика</span>
+          </a>
         </Link>
-        <Link href="/courses">
-          <div className={styles.menuItem}>
-            <Image src="/icons/backpack.svg" width={38} height={38} alt="trophy" />
-          </div>
+        <Link href="/news">
+          <a className={styles.menuItem}>
+            <Image width={25} height={29} src="/icons/news.svg" />
+            <span className={styles.menuItemTitle}>Новости</span>
+          </a>
         </Link>
-        <Link href="/courses">
-          <div className={styles.menuItem}>
-            <Image src="/icons/comment-alt-lines.svg" width={38} height={38} alt="news" />
-          </div>
-        </Link>
-        <Link href="/courses">
-          <div className={notMobileItemClass}>
-            <Image src="/icons/user-secret.svg" width={38} height={38} alt="admin" />
-          </div>
+        <Link href="/admin">
+          <a className={styles.menuItem}>
+            <Image width={25} height={29} src="/icons/admin.svg" />
+            <span className={styles.menuItemTitle}>Админка</span>
+          </a>
         </Link>
       </div>
-      <div className={styles.bottomMenu}>
+      <div className={styles.secondMenu}>
         <Link href="/settings">
-          <div className={styles.menuItem}>
-            <Image src="/icons/cog.svg" width={38} height={38} alt="settings" />
-          </div>
+          <a className={styles.menuItem}>
+            <Image width={25} height={29} src="/icons/cog.svg" />
+            <span className={styles.menuItemTitle}>Настройки</span>
+          </a>
         </Link>
-        <Link href="/courses">
-          <div className={notMobileItemClass}>
-            <Image src="/icons/question-circle.svg" width={38} height={38} alt="faq" />
-          </div>
+        <Link href="/faq">
+          <a className={styles.menuItem}>
+            <Image width={25} height={29} src="/icons/question-circle.svg" />
+            <span className={styles.menuItemTitle}>Вопросы</span>
+          </a>
         </Link>
       </div>
-    </aside>
+    </nav>
   );
 }
