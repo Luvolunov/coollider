c397f50b93c79d0ac66b206d3fcadd218d929527
Vladimir Ivanov <luvolunov@gmail.com>
2021-05-28 22:03:40 +0900

    cp-feature: added loader

--- shared/components/page-container/page-container.component.tsx
+++ shared/components/page-container/page-container.component.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import Header from '../header/header.component';
 import Menu from '../menu/menu.component';
 import styles from './page-container.module.scss';
+import UserAPI from '../../api/user.api';
+import Loader from '../loader/loader.component';
 
 type PageContainerProps = {
   children: any;
 };
 
 export default function PageContainer({ children }: PageContainerProps) {
-  return (
+  const { data } = UserAPI.current();
+  return !data ? <Loader /> : (
     <div className={styles.container}>
       <Header />
       <Menu />
