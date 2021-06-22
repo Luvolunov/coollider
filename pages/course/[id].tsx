d57d6311351435738639023a4f62345b04c3ab42
Vladimir Ivanov <luvolunov@gmail.com>
2021-06-22 21:51:46 +0900

    cp-65: added frame for the course card page

--- pages/course/[id].tsx
+++ pages/course/[id].tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
+import styles from './course.module.scss';
 import { setTitle } from '../../store/title';
 import Card from '../../shared/components/card/card.component';
 import { CourseInterface } from '../../shared/types/course.interface';
@@ -23,9 +24,31 @@ export default function Course({ course }: CourseProps) {
         </title>
       </Head>
       <Card>
-        <img width={150} height={150} src={course.imageUrl} alt={course.name} />
-        <span>{course?.name}</span>
+        <div className={styles.container}>
+          <img width={150} height={150} src={course.imageUrl} alt={course.name} />
+          <div className={styles.info}>
+            <span className={styles.title}>{course?.name}</span>
+            <span className={styles.description}>
+              Матема́тика — наука о структурах, порядке и отношениях, исторически сложившаяся
+              на основе операций подсчёта, измерения и описания формы объектов.
+            </span>
+          </div>
+        </div>
+        <br />
+        <br />
+        <Card>
+          <b>Lesson 1</b>
+          <br />
+          <span className={styles.description}>Just testing you know</span>
+        </Card>
+        <br />
+        <Card>
+          <b>Lesson 2</b>
+          <br />
+          <span className={styles.description}>Another testing, boi</span>
+        </Card>
       </Card>
+      <br />
     </>
   );
 }
