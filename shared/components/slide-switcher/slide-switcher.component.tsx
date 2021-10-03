139455ca15b786e28b70d10f2d37c2184f994a0a
Vladimir Ivanov <luvolunov@gmail.com>
2021-10-03 20:49:11 +0900

    cp: finally added tests

--- shared/components/slide-switcher/slide-switcher.component.tsx
+++ shared/components/slide-switcher/slide-switcher.component.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
 import { Slide } from '../../types/block.interface';
 import { SlideType } from '../../types/slide-type.enum';
 import 'react-quill/dist/quill.bubble.css';
 import styles from './slide-switcher.module.scss';
+import classnames from 'classnames';
 
 type SlideSwitcherProps = {
   slide: Slide;
-  onAnswer: () => void
+  onAnswer: (correct?: boolean) => void
 };
 
 const quillModules = {
@@ -29,6 +30,27 @@ const ReactQuillWithNoSSR = dynamic(() => import('react-quill'), {
 });
 
 export default function SlideSwitcher({ slide, onAnswer }: SlideSwitcherProps) {
+  const [answered, setAnswered] = useState(false);
+  const [correct, setCorrect] = useState(false);
+  const answerHandler = (id: number) => {
+    setCorrect(slide.content.correctVariantId === id);
+    setAnswered(true);
+  };
+  const gradeClasses = classnames(styles.grade, {
+    [styles.show]: answered,
+    [styles.correct]: correct,
+  });
+  const variantsClasses = classnames(styles.variants, {
+    [styles.hide]: answered,
+  });
+  useEffect(() => {
+    if (!answered) { return; }
+    onAnswer();
+  }, [answered]);
+  useEffect(() => {
+    setAnswered(false);
+    setCorrect(false);
+  }, [slide]);
   switch (slide.type) {
     case SlideType.Text: {
       return (
@@ -45,12 +67,26 @@ export default function SlideSwitcher({ slide, onAnswer }: SlideSwitcherProps) {
       return (
         <div className={styles.testSlideOuter}>
           <span className={styles.question}>{slide.content.question}</span>
-          <div className={styles.variants}>
+          <div className={gradeClasses}>
+            {
+              correct ? 'Верно' : 'Неверно'
+            }
+          </div>
+          <div className={variantsClasses}>
             {
               slide.content.variants.map((variant: { id: number, text: string }) => (
-                <button type="button" onClick={onAnswer} className={styles.variant}>
-                  {variant.text}
-                </button>
+                <React.Fragment key={variant.id}>
+                  <input
+                    className={styles.radio}
+                    onChange={() => answerHandler(variant.id)}
+                    type="radio"
+                    name="variant"
+                    id={`variant-${variant.id}`}
+                  />
+                  <label htmlFor={`variant-${variant.id}`} className={styles.variant}>
+                    {variant.text}
+                  </label>
+                </React.Fragment>
               ))
             }
           </div>
