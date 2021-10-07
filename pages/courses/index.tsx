000f51b30ca461a539d9d345aab0a3dd75ec5b69
Vladimir Ivanov <luvolunov@gmail.com>
2021-10-07 15:25:11 +0900

    cp: added description for courses, added favicon

--- pages/courses/index.tsx
+++ pages/courses/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import Link from 'next/link';
+import Head from 'next/head';
 import Greetings from '../../shared/components/greetings/greetings.component';
 import { setTitle } from '../../store/title';
 import Course from '../../shared/components/course/course.component';
@@ -13,21 +14,27 @@ export default function CoursesPage() {
     setTitle('Курсы');
   });
   return (
-    <section>
-      <Greetings />
-      <div className={styles.coursesList}>
-        {
-          data?.map((course: CourseInterface) => (
-            <div key={`${course.authorId}_${course.id}`} className={styles.courseOuter}>
-              <Link href="/course/[id]" as={`/course/${course.id}`}>
-                <div>
-                  <Course course={course} />
-                </div>
-              </Link>
-            </div>
-          ))
-        }
-      </div>
-    </section>
+    <>
+      <Head>
+        <title>Курсы | Куллайдер</title>
+        <meta name="description" content="Начни изучение веб-разработки вместе с Куллайдером прямо сейчас!" />
+      </Head>
+      <section>
+        <Greetings />
+        <div className={styles.coursesList}>
+          {
+            data?.map((course: CourseInterface) => (
+              <div key={`${course.authorId}_${course.id}`} className={styles.courseOuter}>
+                <Link href="/course/[id]" as={`/course/${course.id}`}>
+                  <div>
+                    <Course course={course} />
+                  </div>
+                </Link>
+              </div>
+            ))
+          }
+        </div>
+      </section>
+    </>
   );
 }
