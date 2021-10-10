79b8a95d8f262a83589b3a433921debfb2e02d63
Vladimir Ivanov <luvolunov@gmail.com>
2021-10-10 18:31:42 +0900

    cp: added lessons sorting

--- shared/utils/get-course.function.ts
+++ shared/utils/get-course.function.ts
@@ -4,10 +4,12 @@ import { ApiResponse } from '../types/api-response.interface';
 import { CourseInterface } from '../types/course.interface';
 
 export async function getCourse({ req, params }: GetServerSidePropsContext, admin = false) {
-  const response = await fetch(buildUrl(`/course/${admin ? 'admin/' : ''}${params?.id}`), {
+  const lessonResponse = await fetch(buildUrl(`/course/${admin ? 'admin/' : ''}${params?.id}`), {
     headers: {
       cookie: req.headers.cookie as string,
     },
   });
-  return await response.json() as ApiResponse<CourseInterface>;
+  const response = await lessonResponse.json() as ApiResponse<CourseInterface>;
+  response.body.lessons?.sort((lesson, nextLesson) => lesson.order - nextLesson.order);
+  return response;
 }
