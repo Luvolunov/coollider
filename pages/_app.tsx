a8fa044c860ff8a0db1abdd9afc88a2e842fd579
Vladimir Ivanov <luvolunov@gmail.com>
2021-12-02 16:22:31 +0900

    cp: added knowledge check frame

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,9 +18,7 @@ if (process.browser) {
 
 export default function Coollider({ Component, pageProps }: AppProps) {
   const router = useRouter();
-  const isAuth = /auth/.test(router.route);
-  const isError = /404|500/.test(router.route);
-  const isLesson = /\/lesson\//.test(router.route);
+  const canBeShown = !/(auth|404|500|\/lesson\/|knowledge-check)/.test(router.route);
   const [navigating, setNavigating] = useState(false);
   if (process.browser && 'serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js');
@@ -62,7 +60,7 @@ export default function Coollider({ Component, pageProps }: AppProps) {
           : null
       }
       {
-        !isAuth && !isError && !isLesson
+        canBeShown
           ? (
             <DndProvider backend={HTML5Backend}>
               <PageContainer>
